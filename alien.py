8132e75d752cd52715fa98ebeb47cc8061350d7e
cumbinar <cumbiweb@gmail.com>
2022-06-14 16:19:04 -0500

    se crea class alien

--- alien.py
+++ alien.py
@@ -6,4 +6,12 @@ class alien(Sprite):
     def __init__(self, a_game):
         super().__init__()
         self.screen = a_game.screen
+
+        self.image = pygame.image.load("imagenes/alien.png")
+        self.rect = self.image.getrect()
+
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        self.x = float(self.rect.x)
         
