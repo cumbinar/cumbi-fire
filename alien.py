bb7200d74040fdbd5658c9af706a1cee42343ea9
cumbinar <cumbiweb@gmail.com>
2022-06-14 16:50:49 -0500

    actualiza class Alien

--- alien.py
+++ alien.py
@@ -2,13 +2,13 @@ import pygame
 from pygame.sprite import Sprite
 
 
-class alien(Sprite):
+class Alien(Sprite):
     def __init__(self, a_game):
         super().__init__()
         self.screen = a_game.screen
 
         self.image = pygame.image.load("imagenes/alien.png")
-        self.rect = self.image.getrect()
+        self.rect = self.image.get_rect()
 
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
