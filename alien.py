823d5cfc5a04b04adf0d39e40681d27bbf7f85bf
cumbinar <cumbiweb@gmail.com>
2022-06-17 07:10:05 -0500

    se delimita en ventana posición flota aliens

--- alien.py
+++ alien.py
@@ -11,12 +11,19 @@ class Alien(Sprite):
         self.rect = self.image.get_rect()
 
         self.rect.x = self.rect.width
-        self.rect.y = self.rect.height
+        self.juego = a_game
 
         self.x = float(self.rect.x)
         self.velocidad_Alien = a_game.velocidad_Alien
 
     def update(self):
-        self.x += self.velocidad_Alien
-        self.rect.x = self.x    
+        self.x += (self.juego.velocidad_Alien * self.juego.flota_direccion)
+        self.rect.x = self.x  
+
+    def checa_bordes(self):
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+
+
         
