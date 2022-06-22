ee0b04a0733895ae3eb96433152f4178bee17f7c
cumbinar <cumbiweb@gmail.com>
2022-06-22 06:51:18 -0500

    se crea archivo estadisticas.py para definir 3 vidas

---
+++ estadisticas.py
@@ -0,0 +1,9 @@
+import pygame
+
+class Estadisticas:
+
+    def __init__(self, a_game):
+        self.reinicia()
+        self.juego = a_game
+    def reinicia(self):
+        self.naves_restantes = self.juego.naves_restantes   
