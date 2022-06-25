b9af5aae9d10f5aa0aaf75faed33c090af09cd9d
cumbinar <cumbiweb@gmail.com>
2022-06-25 07:55:53 -0500

    se programa detección de colisiones nave aliens

--- estadisticas.py
+++ estadisticas.py
@@ -3,7 +3,9 @@ import pygame
 class Estadisticas:
 
     def __init__(self, a_game):
-        self.reinicia()
+        #self.reinicia()
         self.juego = a_game
+        self.reinicia()
+        
     def reinicia(self):
-        self.naves_restantes = self.juego.naves_restantes   
+        self.naves_restantes = self.juego.naves_restantes
